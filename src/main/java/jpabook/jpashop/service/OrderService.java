06055916fb854749824dffb45121370b85878bc0
Kimhyundo <kk090303@naver.com>
2021-01-15 03:56:13 +0900

    add OrderServiceTest

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -1,9 +1,6 @@
 package jpabook.jpashop.service;
 
-import jpabook.jpashop.domain.Delivery;
-import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.*;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
@@ -16,35 +13,34 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class OrderService {
-
-    private final OrderRepository orderRepository;
     private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
     private final ItemRepository itemRepository;
-    //주문
-    @Transactional
-    public Long order(Long memberId, Long itemId, int count){
 
+    /**
+     * 주문
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
         //엔티티 조회
         Member member = memberRepository.findOne(memberId);
         Item item = itemRepository.findOne(itemId);
-
         //배송정보 생성
         Delivery delivery = new Delivery();
         delivery.setAddress(member.getAddress());
-
+        delivery.setStatus(DeliveryStatus.READY);
         //주문상품 생성
         OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
-
         //주문 생성
         Order order = Order.createOrder(member, delivery, orderItem);
-
         //주문 저장
         orderRepository.save(order);
-
         return order.getId();
     }
 
-    //취소
+    /**
+     * 주문 취소
+     */
     @Transactional
     public void cancelOrder(Long orderId) {
         //주문 엔티티 조회
@@ -52,11 +48,10 @@ public class OrderService {
         //주문 취소
         order.cancel();
     }
-
-    /*
-    //검색
-    public List<Order> findOrders(OrderSerach orderSerach) {
-        return orderRepository.finaALl(orderSerach)
-    }
-     */
+/** 주문 검색 */
+/*
+public List<Order> findOrders(OrderSearch orderSearch) {
+return orderRepository.findAll(orderSearch);
+}
+*/
 }
