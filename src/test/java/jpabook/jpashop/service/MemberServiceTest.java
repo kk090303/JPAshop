2a64a8e69a285871ff53360211db0f5faad9ada7
Kimhyundo <kk090303@naver.com>
2021-01-14 09:30:32 +0900

    add business logic_adjust stock

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -12,9 +12,9 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@Transactional
+@RunWith(SpringRunner.class) //Junit 실행 할 때 spring과 함께 실행할지에 대한 여부
+@SpringBootTest  //스프링부트를 띄운 상태로 테스트할 것인지 여부
+@Transactional   //매 태스트 별로 데이터 조작을 rollback
 public class MemberServiceTest {
     @Autowired
     MemberService memberService;
@@ -23,26 +23,26 @@ public class MemberServiceTest {
 
     @Test
     public void 회원가입() throws Exception {
-//Given
+        //Given
         Member member = new Member();
         member.setName("kim");
-//When
+         //When
         Long saveId = memberService.join(member);
-//Then
+         //Then
         assertEquals(member, memberRepository.findOne(saveId));
     }
 
-    @Test(expected = IllegalStateException.class)
+    @Test(expected = IllegalStateException.class) //예외 발생시 IllegalStateException이어야 한다.
     public void 중복_회원_예외() throws Exception {
-//Given
+        //Given
         Member member1 = new Member();
         member1.setName("kim");
         Member member2 = new Member();
         member2.setName("kim");
-//When
+        //When
         memberService.join(member1);
         memberService.join(member2); //예외가 발생해야 한다.
-//Then
-        fail("예외가 발생해야 한다.");
+        //Then
+        fail("예외가 발생해야 한다."); //fail에 도달할 경우 로그 출력
     }
 }
