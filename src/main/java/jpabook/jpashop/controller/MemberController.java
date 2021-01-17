ab0d1efc3b7e1a4d0bd1e5618468f838a1d522f3
Kimhyundo <kk090303@naver.com>
2021-01-17 13:49:55 +0900

    create member form

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        //에러 발생시 result를 createMemberForm으로 다시 보내줌
+        //createMemberForm에서 발생한 오류에 대해서 출력해주는 과정을 진행한다.
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address =  new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";  //첫번째 화면으로 돌림
+    }
+}
