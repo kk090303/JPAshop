ac118941b224d874d2fe88e4b72d1a09780791e3
Kimhyundo <kk090303@naver.com>
2021-01-21 11:09:41 +0900

    add findMember with API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 //@Controller @ResponseBody
 @RestController
@@ -17,12 +19,43 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+    //회원 조회 V1 : 응답 값으로 엔티티를 직접 외부에 노출
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    //회원 조회 V2 : 응답 값으로 엔티티가 아닌 별도의 DTO 사용
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
+    //회원 저장 V1 : Member 엔티티를 Request Body에 직접 매핑
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
 
+    //회원 저장 V2 : 엔티티 대신에 DTO를 RequestBody에 매핑
     @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
         Member member = new Member();
@@ -33,16 +66,17 @@ public class MemberApiController {
     }
 
     @Data
-    static class CreateMemberRequest{
+    static class CreateMemberRequest {
         @NotEmpty
         private String name;
     }
 
 
+    //회원 수정 V2 : DTO를 RequestBdoy에 매핑 + command와 Query문 요청 기능 분할 구현
     @PutMapping("/api/v2/members/{id}")
     public UpdateMemberResponse updateMemberV2(
             @PathVariable("id") Long id,
-            @RequestBody @Valid UpdateMemberRequest request){
+            @RequestBody @Valid UpdateMemberRequest request) {
 
         memberService.update(id, request.getName()); //command 부분
         Member findMember = memberService.findOne(id); //query문 요청 부분
@@ -50,19 +84,19 @@ public class MemberApiController {
     }
 
     @Data
-    static class UpdateMemberRequest{
+    static class UpdateMemberRequest {
         private String name;
     }
 
     @Data
     @AllArgsConstructor
-    static class UpdateMemberResponse{
+    static class UpdateMemberResponse {
         private Long id;
         private String name;
     }
 
     @Data
-    static class CreateMemberResponse{
+    static class CreateMemberResponse {
         private Long id;
 
         public CreateMemberResponse(Long id) {
